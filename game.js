65704404736455ae1c8bc32e5a4113c83f02ff94
eecheng <yucheng871011@gmail.com>
2018-08-10 00:48:22 +0800

    generate tiles automatically

--- game.js
+++ game.js
@@ -1,9 +1,15 @@
 var c = document.getElementById("piano");
 var context = c.getContext("2d");
-paintWindow();
 
+startGame();
+function startGame(){
+    paintWindow();
+    myTiles = [];
+    myTiles[0] = new Block(0);
+    myTiles[1] = new Block(1);
+}
 function paintWindow(){
-    var my_gradient = context.createLinearGradient(0,0,0,600);
+    my_gradient = context.createLinearGradient(0,0,0,600);
     my_gradient.addColorStop(0,"rgba(65,234,246,0.6)");
     my_gradient.addColorStop(1,"rgba(254,74,251,0.5)");
 
@@ -34,3 +40,47 @@ function paintWindow(){
     context.strokeStyle = "white";
     context.stroke();
 }
+
+function Block(index){
+    this.index = index;
+    this.appearPos = Math.floor(Math.random()*4);
+   
+    this.width = 70;
+    this.height = 120;
+    this.color = "black";
+    switch(this.appearPos){
+        case 0:
+            this.x = 0;
+            this.y = 0;
+            break;
+        case 1:
+            this.x = 75;
+            this.y = 0;
+            break;
+        case 2:
+            this.x = 152;
+            this.y = 0;
+            break;
+        case 3:
+            this.x = 228;
+            this.y = 0;
+            break;
+    }
+    context.fillStyle = this.color;
+    context.fillRect(this.x,this.y,this.width,this.height);
+    this.interval = setInterval(move,10,this.index);
+}
+function move(index){
+    //context.clearRect(0,0,300,600);
+    //paintWindow();
+    myTiles[index].y += 1;
+    context.fillStyle = "black";
+    context.fillRect(myTiles[index].x,myTiles[index].y,70,120);
+    
+    context.clearRect(myTiles[index].x,myTiles[index].y-2,70,2);
+    context.fillStyle = my_gradient;
+    context.fillRect(myTiles[index].x,myTiles[index].y-2,70,2);
+
+ 
+}
+ 
